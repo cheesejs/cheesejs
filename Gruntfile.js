43326097811bb2cd300e0af86ebea49855eb378f
xinghaidong <xinghaidong@live.com>
2015-12-15 12:51:56 +0800

    add config files

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+/*global module:false*/
+module.exports = function(grunt) {
+	
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+			' Licensed <%= pkg.licenses %> */\n',
+		// Task configuration.
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true
+			},
+			dist: {
+				src: ['lib/<%= pkg.name %>.js'],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		},
+		"bower": {
+			"install": {
+				"options": {
+					"targetDir": "lib",
+					"layout": "byComponent",
+					"install": true,
+					"verbose": false,
+					"cleanTargetDir": false
+				}
+			}
+		}
+
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bower-task');
+
+	// Default task.
+	grunt.registerTask('default', ['concat', 'uglify']);
+
+};
